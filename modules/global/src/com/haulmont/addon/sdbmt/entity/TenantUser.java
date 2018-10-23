af1c62b45ff0c1a0040350be8b87e91a29a9d54e
Evgeny Zaharchenko <zaharchenko@haulmont.com>
2018-10-23 15:57:33 +0400

    To add predefined system entities #4
    (cherry picked from commit adfc9085e0fbb08c3a243f6214c0443bd168d569)

--- modules/global/src/com/haulmont/addon/sdbmt/entity/TenantUser.java
+++ modules/global/src/com/haulmont/addon/sdbmt/entity/TenantUser.java
@@ -9,6 +9,7 @@ package com.haulmont.addon.sdbmt.entity;
 import com.haulmont.addon.sdbmt.core.TenantId;
 import com.haulmont.chile.core.annotations.NamePattern;
 import com.haulmont.cuba.core.entity.annotation.Extends;
+import com.haulmont.cuba.core.entity.annotation.Listeners;
 import com.haulmont.cuba.security.entity.User;
 
 import javax.persistence.Column;
@@ -16,6 +17,7 @@ import javax.persistence.Entity;
 
 @Extends(User.class)
 @Entity(name = "cubasdbmt$TenantUser")
+@Listeners("cubasdbmt_SdbmtUserEntityListener")
 @NamePattern("#getCaption|login,name,tenantId")
 public class TenantUser extends User implements HasTenant {
     private static final long serialVersionUID = -2598354095282194816L;
