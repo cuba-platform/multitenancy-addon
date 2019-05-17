7bbc469b63b31331ff5af8d5d4298421ad6e200a
Nikita Shchienko <shchienko@haulmont.com>
2019-05-17 16:04:49 +0400

    No validation of tenant id in UI #21

---
+++ modules/web/src/com/haulmont/addon/sdbmt/web/tenant/validators/TenantIdValidator.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2008-2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.haulmont.addon.sdbmt.web.tenant.validators;
+
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.components.Field;
+import com.haulmont.cuba.gui.components.ValidationException;
+import org.apache.commons.lang3.StringUtils;
+
+public class TenantIdValidator implements Field.Validator {
+
+    protected Messages messages = AppBeans.get(Messages.class);
+
+    @Override
+    public void validate(Object value) throws ValidationException {
+        if (StringUtils.isNotEmpty((String) value) && !((String) value).matches("[\\w|\\s]+"))
+            throw new ValidationException(messages.getMessage(TenantIdValidator.class, "validation.invalidTenantId"));
+    }
+}
